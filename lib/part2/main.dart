2586374f79110712120a2b70cbb2eb8c8d546d0a
kamagata-ko <eighth.9512@gmail.com>
2023-07-20 00:13:39 +0900

    part2

---
+++ lib/part2/main.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  const a = MaterialApp(
+    home: Scaffold(
+      body: Center(
+        child: Text("banana"),
+      ),
+    ),
+  );
+
+  runApp(a);
+}
